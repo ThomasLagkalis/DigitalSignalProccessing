cfae9b765c46fbbaaa4bbbb07acaf9625c7c1944
gbitsys <paok4geogeo@gmail.com>
2023-11-20 22:49:29 +0200

    Askisi 3 moving on

--- Seira3/Ask3.m
+++ Seira3/Ask3.m
@@ -1,4 +1,30 @@
 clear all; clc;
-fs = 10*10^3; %sampling frequency 10.000Hz
-t = [];
-x(t)=1+cos(1000t)+cos(16000t)+cos(30000t)
+%%% a subquestion %%%
+
+%%% filter from Ask1.m %%%
+fs = 10000;
+Ts = 1/fs;
+Wp = 2*pi*3000; % Frequency passband rad/s
+Ws = 2*pi*4000; % Frequency stopband rad/s
+delta_p = 3;  % Ripple 3 db
+delta_s = 30;   % Attenuation
+
+[N, Wc] = buttord(Wp, Ws, delta_p, delta_s, 's');
+[z, p, k] = buttap(N);
+[num1, den1] = zp2tf(z, p, k);
+[num2, den2] = lp2lp(num1, den1, Wc);
+[NUMz,DENz] = bilinear(num2,den2,fs);
+%%%%%%
+
+samples = linspace(0, 499, 500); %works like discrete time domain
+x1=1+cos(1000*samples*Ts)+cos(1600*samples*Ts)+cos(30000*samples*Ts); %signal x sampled
+figure();
+stem(samples, x1);
+xlabel("n");
+ylabel("x(n)");
+
+x_filtered = filter(NUMz, DENz, x1);
+figure();
+stem(samples, x_filtered);
+
+
