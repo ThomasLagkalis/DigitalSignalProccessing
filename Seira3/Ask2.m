81d3695f03a37318e98d4175f1f652e8e17a42c7
gbitsys <paok4geogeo@gmail.com>
2023-11-20 15:07:46 +0200

    changed Ask2 seira3

--- Seira3/Ask2.m
+++ Seira3/Ask2.m
@@ -7,10 +7,10 @@ d_ripple = 3; %passband ripple in dB
 Ts = 0.2;   %sampling period
 fs = 1/Ts;  %sampling frequency
 fmax = fs/2;
-fc = wc/2*pi;%cutoff frequency hz
+fc = wc/(2*pi);%cutoff frequency hz
 fc_dig = fc/(fs/2); %normalize frequency 
 
-[num1 , den1] = cheby1(N, d_ripple, fc_dig/(2*pi), "high", "z"); %fc_dig*2*pi because we want rad/sec
+[num1 , den1] = cheby1(N, d_ripple, fc_dig, "high", "z"); %fc_dig*2*pi because we want rad/sec
 [hd, wd]=freqz(num1, den1, samples); %hd frequency response , wd angular frequency response
 
 w = [0:1/(samples-1):1]; %normalized
@@ -22,7 +22,7 @@ xlabel("radians per sample");
 hold on;
 %%% b subquestion %%%
 N2= 16;
-[num2 , den2] = cheby1(N2, d_ripple, fc_dig/(2*pi), "high", "z");
+[num2 , den2] = cheby1(N2, d_ripple, fc_dig, "high", "z");
 [hd2, wd2]=freqz(num2, den2, samples);
 plot(w, mag2db(abs(hd2)), "Color", "r", "LineWidth", 1.2);
 title("Frequency response in dB for 256 samples order 16");
---
+++ Seira3/Ask3.m
@@ -0,0 +1,4 @@
+clear all; clc;
+fs = 10*10^3; %sampling frequency 10.000Hz
+t = [];
+x(t)=1+cos(1000t)+cos(16000t)+cos(30000t)
