4ee03f8dbedb966fd6c8f964029b1d4e94927e03
ThomasLagkalis <thomaslagkalis@gmail.com>
2023-12-05 15:52:51 +0200

    Small changes in normalized freq

--- Seira4/Ask1.m
+++ Seira4/Ask1.m
@@ -4,7 +4,7 @@ wc = 0.4*pi; %cut-off frequency rad/sec
 fs = 200; %sampling frequency in Hz
 fc=wc/(2*pi); %cut-off frequency in Hz
 
-wc_norm = fc/(fs/2); %cut-off frequency normalized
+wc_norm = wc/(2*pi*fs/2); %cut-off frequency normalized
 N=21; %window length
 
 H_rect = fir1(N-1, wc_norm, rectwin(N)); %fir filter with rectangular window
